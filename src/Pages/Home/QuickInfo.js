99b2de1582f8f1e44f6ad2a5c36201f1f74b21f4
afzal khan <to.babu.khan@gmail.com>
2023-07-08 18:06:25 +0600

    removed pre project

--- src/Pages/Home/QuickInfo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function QuickInfo() {
-    return (
-        <div className="w-full h-full bg-green-300">
-            <h1>From Quick info .</h1>
-        </div>
-    )
-}
--- src/Pages/Map/Googlemap/LastLocation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function LastLocation(map) {
-    return (
-        <div>
-            
-        </div>
-    )
-}

27555a04815c3d76ddbcc8b778ad1602408d0b24
kawsar007 <imkawsar007@gmail.com>
2024-03-10 02:19:45 +0600

    Added Car search, Single select and multiple select

--- src/components/tracking-ui/vehicle/vehicle-table.jsx
+++ src/components/tracking-ui/vehicle/vehicle-table.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaCarSide } from "react-icons/fa";
 import { HiSearch } from "react-icons/hi";
+import { all_cars } from "../../../db";
 
 const VehicleTable = () => {
+  const [searchCar, setSearchCar] = useState("");
+  const [selectedCar, setSelectedCar] = useState([]);
+  const [selectAll, setSelectAll] = useState(false);
+
+  const handleSearch = (e) => {
+    setSearchCar(e.target.value);
+  };
+
+  // Filter car
+  const filterCar = all_cars.filter((car) => {
+    return car.car_name.toLowerCase().includes(searchCar.toLowerCase());
+  });
+
+  // Handler for selecting and deselecting all cars;
+  const handleSelectAll = () => {
+    if (selectAll) {
+      setSelectedCar([]);
+    } else {
+      setSelectedCar(all_cars.map((car) => car.car_name));
+    }
+    setSelectAll(!selectAll);
+  };
+
+  // Handler for selecting and deselecting a single car
+  const handleSelectCar = (event) => {
+    const carName = event.target.value;
+    if (selectedCar.includes(carName)) {
+      setSelectedCar(selectedCar.filter((name) => name !== carName));
+    } else {
+      setSelectedCar([...selectedCar, carName]);
+    }
+  };
+
   return (
     <div className="mx-auto max-w-2xl">
       <div className="relative shadow-md sm:rounded-lg">
@@ -19,6 +53,8 @@ const VehicleTable = () => {
               id="table-search"
               className="block w-80 rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
               placeholder="Search for items"
+              value={searchCar}
+              onChange={handleSearch}
             />
           </div>
         </div>
@@ -31,6 +67,8 @@ const VehicleTable = () => {
                   <input
                     id="checkbox-all-search"
                     type="checkbox"
+                    checked={selectAll}
+                    onChange={handleSelectAll}
                     className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
                   />
                   <label htmlFor="checkbox-all-search" className="sr-only">
@@ -50,7 +88,47 @@ const VehicleTable = () => {
             </tr>
           </thead>
           <tbody className="bg-white flex flex-col items-center justify-start overflow-y-scroll w-full h-[200px]">
-            <tr className="flex justify-between items-center h-[40px] w-full text-black border-b bg-white hover:bg-gray-50">
+            {filterCar.map((car, index) => (
+              <tr
+                key={index}
+                className="flex justify-between items-center h-[40px] w-full text-black border-b bg-white hover:bg-gray-50"
+              >
+                <td className="w-4 p-4">
+                  <div className="flex items-center">
+                    <input
+                      id="checkbox-table-search-1"
+                      type="checkbox"
+                      value={car?.car_name}
+                      checked={selectedCar.includes(car?.car_name)}
+                      onChange={handleSelectCar}
+                      className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                    />
+                    <label
+                      htmlFor="checkbox-table-search-1"
+                      className="sr-only"
+                    >
+                      checkbox
+                    </label>
+                  </div>
+                </td>
+                <th
+                  scope="row"
+                  className="!text-left whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
+                >
+                  {car?.car_name}
+                </th>
+                <td className="px-6 py-4">{car?.speed}</td>
+                <td className="px-6 py-4 text-right">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    <FaCarSide />
+                  </a>
+                </td>
+              </tr>
+            ))}
+            {/* <tr className="flex justify-between items-center h-[40px] w-full text-black border-b bg-white hover:bg-gray-50">
               <td className="w-4 p-4">
                 <div className="flex items-center">
                   <input
@@ -223,7 +301,7 @@ const VehicleTable = () => {
                   <FaCarSide />
                 </a>
               </td>
-            </tr>
+            </tr> */}
             {/* <tr className="flex w-full mb-4 text-black border-b bg-white hover:bg-gray-50">
               <td className="w-4 p-4">
                 <div className="flex items-center">
---
+++ src/db.js
@@ -0,0 +1,12 @@
+export const all_cars = [
+  { car_name: "Toyota", speed: 120, active: true },
+  { car_name: "Civic", speed: 110, active: false },
+  { car_name: "Ford", speed: 180, active: true },
+  { car_name: "Chevrolet", speed: 200, active: false },
+  { car_name: "BMW M3", speed: 190, active: true },
+  { car_name: "Audi A4", speed: 150, active: false },
+  { car_name: "Mercedes", speed: 170, active: true },
+  { car_name: "Lexus", speed: 140, active: false },
+  { car_name: "Subaru", speed: 100, active: true },
+  { car_name: "Tesla", speed: 220, active: false },
+];
